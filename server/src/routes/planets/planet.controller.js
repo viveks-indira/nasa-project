b1bebe390455a65c7ec7c391ef567049392557ed
viveks-indira <viveks@indiratrade.com>
2024-04-26 13:26:42 +0530

    build

--- server/src/routes/planets/planet.controller.js
+++
@@ -1,9 +0,0 @@
-const { getAllPlanets } = require('../../models/planet.model');
-
-async function httpGetAllPlanets(req, res) {
-  return res.status(200).json(await getAllPlanets());
-}
-
-module.exports = {
-  httpGetAllPlanets,
-};
